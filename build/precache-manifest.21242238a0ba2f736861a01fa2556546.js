2a6638ce841435ea6fe934f95c10dd8aff8ab455
Gerard Genovese <gmoneymaster24@yahoo.com>
2019-02-28 14:51:04 -0500

    first heroku build

---
+++ build/precache-manifest.21242238a0ba2f736861a01fa2556546.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = [
+  {
+    "revision": "46d7d2ba9f6bbd182e25af6dfc3a40a4",
+    "url": "/static/media/x6.46d7d2ba.jpg"
+  },
+  {
+    "revision": "91a3811b2a6747d0ae82",
+    "url": "/static/css/main.5beda588.chunk.css"
+  },
+  {
+    "revision": "2e4159bf356204f2282bb6f85517b867",
+    "url": "/static/media/tcare2.2e4159bf.jpg"
+  },
+  {
+    "revision": "4cebfd30f5149b33ef4a",
+    "url": "/static/js/1.4cebfd30.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5fe876d983f3150310bf0a1f4cf7ba7b",
+    "url": "/static/media/side.5fe876d9.PNG"
+  },
+  {
+    "revision": "fd9563da7145434be45eb0729db94532",
+    "url": "/static/media/front.fd9563da.PNG"
+  },
+  {
+    "revision": "b51c45a447c0bdee04cefdff70f5d356",
+    "url": "/static/media/side.b51c45a4.PNG"
+  },
+  {
+    "revision": "51b41766227f45cdcb6d33b5962a08b7",
+    "url": "/static/media/front.51b41766.PNG"
+  },
+  {
+    "revision": "1447206943a43505cddacd685a98e114",
+    "url": "/static/media/side.14472069.PNG"
+  },
+  {
+    "revision": "ba1c6533d57c49a3be97500ad6e04e67",
+    "url": "/static/media/front.ba1c6533.PNG"
+  },
+  {
+    "revision": "436621ffa3606125e0717ab7e716b21c",
+    "url": "/static/media/side.436621ff.png"
+  },
+  {
+    "revision": "d409d08fe13b6ee28bf5c9d176567948",
+    "url": "/static/media/front.d409d08f.png"
+  },
+  {
+    "revision": "cb8fdc89787ed33bd598db2bae9e7d30",
+    "url": "/static/media/side.cb8fdc89.png"
+  },
+  {
+    "revision": "34e325aaf295f88290b3792519e188d2",
+    "url": "/static/media/front.34e325aa.png"
+  },
+  {
+    "revision": "045bf364227711f0266708c6fc639489",
+    "url": "/static/media/toyota-logo.045bf364.PNG"
+  },
+  {
+    "revision": "7ee227a90ff36bdcfb46b2602e12f943",
+    "url": "/static/media/supra.7ee227a9.jpg"
+  },
+  {
+    "revision": "cc4f4ed68929be55fa59bed0782b3fec",
+    "url": "/static/media/x0.cc4f4ed6.jpg"
+  },
+  {
+    "revision": "d0c25ad313ce539afc1534a086e25626",
+    "url": "/static/media/x1.d0c25ad3.jpg"
+  },
+  {
+    "revision": "c9bffd81acc0a4dd4121d6d9ae5fd5bc",
+    "url": "/static/media/x2.c9bffd81.jpg"
+  },
+  {
+    "revision": "c3342d7bdd25076b7bd7976012945287",
+    "url": "/static/media/x3.c3342d7b.jpg"
+  },
+  {
+    "revision": "48e6148decdc0353dc821296f292509d",
+    "url": "/static/media/x4.48e6148d.jpg"
+  },
+  {
+    "revision": "27ece7d3553b1e4c13b481eee38d185d",
+    "url": "/static/media/x5.27ece7d3.jpg"
+  },
+  {
+    "revision": "91a3811b2a6747d0ae82",
+    "url": "/static/js/main.91a3811b.chunk.js"
+  },
+  {
+    "revision": "b00cecdc812b7ba8ae74424f3a8441bd",
+    "url": "/static/media/x7.b00cecdc.jpg"
+  },
+  {
+    "revision": "f34b37ba0340ba144a41398500a18bb5",
+    "url": "/static/media/kbb-award.f34b37ba.png"
+  },
+  {
+    "revision": "1483bc09814e2bef76fe993fa22ed25b",
+    "url": "/static/media/img1.1483bc09.jpg"
+  },
+  {
+    "revision": "98845bd7ff79caaa8236b79f30e792f8",
+    "url": "/static/media/img2.98845bd7.jpg"
+  },
+  {
+    "revision": "926d2c32e7eaec2651f94bf38c57d8d6",
+    "url": "/static/media/img3.926d2c32.jpg"
+  },
+  {
+    "revision": "5991f637ef074d6a1da30b89a0659385",
+    "url": "/static/media/img4.5991f637.jpg"
+  },
+  {
+    "revision": "4720ddcc4342e92255bd1064cc7cdb54",
+    "url": "/static/media/img5-long.4720ddcc.jpg"
+  },
+  {
+    "revision": "1213deaa264109ff7bfcb2267798bcff",
+    "url": "/static/media/img6.1213deaa.jpg"
+  },
+  {
+    "revision": "05e49390b35bfec467a1b3441f64ee7e",
+    "url": "/static/media/img7.05e49390.jpg"
+  },
+  {
+    "revision": "79c2cde8aae0c353a94363e19f44d651",
+    "url": "/static/media/img8.79c2cde8.jpg"
+  },
+  {
+    "revision": "eb4a4f4a68f5bca7e9d30da2b79b27b2",
+    "url": "/static/media/rickrolled.eb4a4f4a.mp4"
+  },
+  {
+    "revision": "73f0b07f5bbb04b6423a57525d730c8c",
+    "url": "/static/media/cargo-tote.73f0b07f.jpg"
+  },
+  {
+    "revision": "ad4cabc7679eaae0ea993381b5737846",
+    "url": "/static/media/leather-mats.ad4cabc7.jpg"
+  },
+  {
+    "revision": "c9e0ad68bce4b7a7c5082337dcdf3913",
+    "url": "/static/media/wheel-lock.c9e0ad68.jpg"
+  },
+  {
+    "revision": "8310aa181c9567ec4fa4075ad003ddc4",
+    "url": "/static/media/loading.8310aa18.gif"
+  },
+  {
+    "revision": "ae21f06a5cfad6402870a93914c03486",
+    "url": "/static/media/toyotaVideo1.ae21f06a.mp4"
+  },
+  {
+    "revision": "b3b747a21ac5fe1853d4779e71b36f10",
+    "url": "/static/media/toyotaVideo2.b3b747a2.mp4"
+  },
+  {
+    "revision": "63b1c5a5038484db40938617cb755dba",
+    "url": "/static/media/xfinity.63b1c5a5.jpg"
+  },
+  {
+    "revision": "180836ca0bf3cd1d22c7c703b98559cf",
+    "url": "/static/media/racingConcept.180836ca.jpg"
+  },
+  {
+    "revision": "01b3cd3c63eddbcf17830546663748bb",
+    "url": "/static/media/tcareheader.01b3cd3c.jpg"
+  },
+  {
+    "revision": "b110e9bae179fece771b4716019b04d9",
+    "url": "/static/media/tcare1.b110e9ba.jpg"
+  },
+  {
+    "revision": "4cebfd30f5149b33ef4a",
+    "url": "/static/css/1.bbbdf1af.chunk.css"
+  },
+  {
+    "revision": "d4d01e2e3e54dc79900a7ba406ab7686",
+    "url": "/index.html"
+  }
+];
