2286cfc91ed0dc0371b8f5463f8942ebcd0b3b3d
Gerard Genovese <gmoneymaster24@yahoo.com>
2019-02-28 15:37:46 -0500

    capitalized folder slideShow in reducer but slideshow in folder, heroku pics missing

--- src/redux/reducers/slideShowReducer.js
+++ src/redux/reducers/slideShowReducer.js
@@ -2,7 +2,7 @@ export default () => {
   return [
     { 
       model: "avalon", 
-      img: "/images/slideShow/avalonSlide.png",
+      img: "/images/slideshow/avalonslide.png",
       carAngle: 0,
       color:"blue",
       engine: 0,
@@ -10,12 +10,12 @@ export default () => {
       miles: "25/30",
       perMonth: 450,
       signing: 3000,
-      background: "/images/slideShow/background1.jpg",
+      background: "/images/slideshow/background1.jpg",
       buildCarImg: "/images/avalon/sidefront/blue.jpg"
     },
     { 
       model: "camry", 
-      img: "/images/slideShow/camrySlide.png",
+      img: "/images/slideshow/camryslide.png",
       carAngle: 0,
       color:"red",
       engine: "0",
@@ -23,12 +23,12 @@ export default () => {
       miles: "29/32",
       perMonth: 350,
       signing: 2500,
-      background: "/images/slideShow/background2.jpg",
+      background: "/images/slideshow/background2.jpg",
       buildCarImg: "/images/camry/sidefront/red.jpg"
     },
     { 
       model: "corolla", 
-      img: "/images/slideShow/corollaSlide.png",
+      img: "/images/slideshow/corollaslide.png",
       carAngle: 0,
       color:"white",
       engine: "0",
@@ -36,12 +36,12 @@ export default () => {
       miles: "28/35",
       perMonth: 300,
       signing: 2000,
-      background: "/images/slideShow/background3.jpg",
+      background: "/images/slideshow/background3.jpg",
       buildCarImg: "/images/corolla/sidefront/white.jpg"
     },
     { 
       model: "86", 
-      img: "/images/slideShow/86Slide.png",
+      img: "/images/slideshow/86Slide.png",
       carAngle: 0,
       color:"white",
       engine: "0",
@@ -49,7 +49,7 @@ export default () => {
       miles: "22/28",
       perMonth: 375,
       signing: 2800,
-      background: "/images/slideShow/background4.jpg",
+      background: "/images/slideshow/background4.jpg",
       buildCarImg: "/images/86/sidefront/white.jpg"
     }
   ]
